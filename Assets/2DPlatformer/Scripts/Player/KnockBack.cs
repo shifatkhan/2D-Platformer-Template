af66a801d5672d2d0d804dd7bb534118f01cdd65
Shifat Khan <shifatkhan6@gmail.com>
2020-05-15 19:13:38 -0400

    Added Player animations and enemy

---
+++ Assets/2DPlatformer/Scripts/Player/KnockBack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockBack : MonoBehaviour
+{
+    public float thrust;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody2D enemy = other.GetComponent<Rigidbody2D>();
+            if(enemy != null)
+            {
+                // Make rigidbody dynamic so we can add a knock back force.
+                enemy.isKinematic = false;
+
+                Vector2 direction = enemy.transform.position - transform.position;
+                direction = direction.normalized * thrust;
+                enemy.AddForce(direction, ForceMode2D.Impulse);
+
+                enemy.isKinematic = true;
+            }
+        }
+    }
+}
